e1bb80eac9f9ec925d4b84f839dccad3f8d1b865
chagy <chagy_x_@hotmail.com>
2021-05-21 11:05:55 +0700

    74 Pagination

--- controllers/product.ts
+++ controllers/product.ts
@@ -1,10 +1,30 @@
-import { RouterMiddleware } from "../deps.ts";
+import { RouterMiddleware, helpers } from "../deps.ts";
 import { runQuery } from '../db/db.ts'
-import { fetchProducts } from '../db/query.ts';
+import { fetchProducts, countProducts } from '../db/query.ts';
 
 export const listProducts: RouterMiddleware = async (ctx) => {
-    const result = await runQuery(fetchProducts())
-    const products = result.rows
+    try {
+        const { l, q } = helpers.getQuery(ctx) as { l?: string; q?: string }
 
-    ctx.response.body = { products }
+
+        const countResult = await runQuery<{ count: bigint }>(countProducts())
+        const countData = countResult.rows[0]
+        const count = Number(countData.count)
+
+        const limit = l ? +l : 3
+
+        const currentQuery = q ? +q : 1
+        const skip = (currentQuery - 1) * limit
+
+        const result = await runQuery(fetchProducts(limit, skip))
+        const products = result.rows
+
+        const totalQueries = Math.ceil(count / limit)
+
+        const hasMore = currentQuery + 1 <= totalQueries
+
+        ctx.response.body = { products, totalQueries, hasMore }
+    } catch (error) {
+        throw error
+    }
 }
